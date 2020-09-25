8aaab2f0e42290fdd867cd1b76aff1a60e95a289
ps807 <66031843+ps807@users.noreply.github.com>
2020-09-25 18:23:25 +0200

    END

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
+
+import * as pp from "./math.js";
+console.log(pp);
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
